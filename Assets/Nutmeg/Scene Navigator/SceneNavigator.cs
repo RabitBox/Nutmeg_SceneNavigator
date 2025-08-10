8ed3acfe23dac1c0180e9b27219c94a9e9c9ac2a
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2025-08-10 22:02:12 +0900

    シーンの読み込みに必要なデータセットを作成

---
+++ Assets/Nutmeg/Scene Navigator/SceneNavigator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Nutmeg
+{
+	public partial class SceneNavigator : MonoBehaviour
+	{
+		public SceneNavigator Instance { get; private set; }
+
+		[Header("Assets")]
+		/// <summary>
+		/// シーンコンフィグデータ
+		/// </summary>
+		[SerializeField] private SceneConfigObject _config;
+
+		[Header("Runtime")]
+		/// <summary>
+		/// 読み込み済シーン一覧
+		/// </summary>
+		[SerializeField] private List<string> _scenes;
+
+		private void Awake()
+		{
+			// シングルトン化
+			Instance = this;
+
+			// シーン一覧を更新
+			RefreshLoadedScenes();
+		}
+
+		/// <summary>
+		/// 読み込み済みシーン一覧を更新
+		/// </summary>
+		private void RefreshLoadedScenes()
+		{
+			_scenes.Clear();
+			for (int i = 0; i < SceneManager.sceneCount; i++)
+			{
+				var scene = SceneManager.GetSceneAt(i);
+				if (this.gameObject.scene.name != scene.name)
+				{
+					_scenes.Add(scene.name);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Nutmeg/Scriptable/SceneConfigObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nutmeg
+{
+	[CreateAssetMenu(fileName = "SceneConfig", menuName = "Nutmeg/SceneConfigObject")]
+	public class SceneConfigObject : ScriptableObject
+	{
+		[System.Serializable]
+		public struct SceneBundle
+		{
+			public string Name;
+			public List<string> SceneNames;
+		}
+
+		/// <summary>
+		/// 実行中永続的に残るシーン一覧
+		/// </summary>
+		[field: SerializeField] public List<string> Permanents;
+
+		/// <summary>
+		/// シーンの組み合わせ一覧
+		/// </summary>
+		[field: SerializeField] public List<SceneBundle> SceneBundles;
+	}
+}
