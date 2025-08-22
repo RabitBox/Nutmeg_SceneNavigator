5c5daedfd368571853d3bf28f26b64de0ad4fa69
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2025-08-22 21:41:24 +0900

    ライセンス適応

--- Assets/Nutmeg/Scene Navigator/SceneNavigator.cs
+++ Assets/Nutmeg/Scene Navigator/SceneNavigator.cs
@@ -1,4 +1,18 @@
-﻿using System.Collections.Generic;
+﻿// zlib/libpng License
+//
+// Copyright (c) 2025 RabitBox
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -17,7 +31,7 @@ namespace Nutmeg
 
 		[Header("Runtime")]
 		/// <summary>
-		/// 読み込み済シーン一覧
+		/// 現在の読み込み済シーン一覧
 		/// </summary>
 		[SerializeField] private List<string> _scenes;
 
@@ -25,6 +39,11 @@ namespace Nutmeg
 		/// 非同期処理ハンドル
 		/// </summary>
 		private HandleQueue _handle = null;
+
+		/// <summary>
+		/// このスクリプトを持つゲームオブジェクトの存在するシーン名
+		/// </summary>
+		private string Current => this.gameObject.scene.name;
 
 		private void Awake()
 		{
@@ -59,11 +78,13 @@ namespace Nutmeg
 		/// <returns></returns>
 		private bool IsPermanent(string sceneName)
 		{
-			if (this.gameObject.scene.name == sceneName)
+			// 常駐シーンは矯正除外
+			if (Current == sceneName)
 			{
 				return true;
 			}
 
+			// 常駐リストと一致するものは除外
 			foreach(var item in _config.Permanents)
 			{
 				if (item == sceneName)
