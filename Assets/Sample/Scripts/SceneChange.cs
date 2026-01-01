18887d6ae24931b75e7e98f0eb17f6eda4bc6211
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2026-01-02 00:16:46 +0900

    [check] シーンロードの動作確認

--- Assets/Sample/Scripts/SceneChange.cs
+++ Assets/Sample/Scripts/SceneChange.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using RV.SpiceKit.Nutmeg;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //Nutmeg.SceneNavigator.Instance.LoadSceneBundleAsync("Main");
-        //Nutmeg.SceneNavigator.Instance.UnloadSceneAsync("SUB");
+    [SerializeField] private SceneFlowController sceneFlowController;
 
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+		//Nutmeg.SceneNavigator.Instance.LoadSceneBundleAsync("Main");
+		//Nutmeg.SceneNavigator.Instance.UnloadSceneAsync("SUB");
+		sceneFlowController?.LoadSceneBundleAsync("Main");
 	}
 
 	// Update is called once per frame
