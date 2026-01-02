86497025d818c4e2f80f275d1300b94bf56fafb9
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2026-01-02 17:52:54 +0900

    [check] ロード時の処理順番チェック

--- Assets/Sample/Scripts/SceneChange.cs
+++ Assets/Sample/Scripts/SceneChange.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
-using RV.SpiceKit.Nutmeg;
-using UnityEngine.UI;
+﻿using RV.SpiceKit.Nutmeg;
+using RV.SpiceKit.Nutmeg.Messages;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using ZeroMessenger;
 
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private SceneFlowController sceneFlowController;
 	[SerializeField] private TMP_Dropdown	dropdown;
 	[SerializeField] private Button			button;
+
+	private void Awake()
+	{
+		MessageBroker<LoadingPhaseChanged>.Default.Subscribe(p => {
+			Debug.Log($"読み込み: {p.Current}");
+		});
+	}
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
