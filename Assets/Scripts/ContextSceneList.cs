5e1a9026ed8b19d9a080912664ccd13025c4da3e
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2026-03-23 13:34:01 +0900

    ScriptableObjectを使用したデータ設計を見直し

---
+++ Assets/Scripts/ContextSceneList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RV.SpiceKit.Nutmeg.SO
+{
+	[CreateAssetMenu(fileName = "ContextSceneList", menuName = "Nutmeg/Scriptable Objects/ContextSceneList")]
+	public class ContextSceneList : ScriptableObject
+	{
+		[System.Serializable]
+		public struct ContextPair
+		{
+			public string Name;
+			public string Value;
+		}
+
+		[field: SerializeField]
+		public List<ContextPair> SceneList { get; private set; }
+	}
+}
+
---
+++ Assets/Scripts/LayerSceneList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RV.SpiceKit.Nutmeg.SO
+{
+	[CreateAssetMenu(fileName = "LayerSceneList", menuName = "Nutmeg/Scriptable Objects/LayerSceneList")]
+	public class LayerSceneList : ScriptableObject
+	{
+		[field : SerializeField]
+		public List<string> SceneList { get; private set; }
+	}
+}
